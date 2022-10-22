864d33553a6cf63fdf30586ae648145e2b84a8b6
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-22 15:37:33 -0500

    Learning components

--- my-first-project/src/index.js
+++ my-first-project/src/index.js
@@ -4,4 +4,16 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<h1>Hello World, Welcome to my first React Project! 🔥🔥❤️</h1>);
+function Gretting () {
+    return <div>
+        <h1>This is a h1 in a div</h1>
+        <p>Mw2</p>
+        </div>;
+};
+
+root.render(<div>
+    <Gretting/>
+    <Gretting/>
+    <Gretting/>
+</div>);
+
