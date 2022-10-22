daa85739ec8e590a20395aecca5ba885ca23aaa0
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-22 18:00:24 -0500

    Learning import/export

--- my-first-project/src/index.js
+++ my-first-project/src/index.js
@@ -1,39 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Gretting, UserCard } from './Greeting';
+import Product, {Navbar} from './Product';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Gretting() {
-    // const married = true;
-    // const age = 18;
-
-    // if (married) {
-    //     return <h1> Estoy casado!</h1>
-    // };
-
-    //Ternary operators.
-    // return <h1>{married ? 'Estoy casado! 🥴' : 'No estoy casado'}</h1>
-
-    const User = {
-        name: 'John',
-        age: 18,
-        married: true
-    };
-
-    function add(x, y) {
-        return x + y;
-    };
-
-    return <>
-        <h1>{User.name}</h1>
-        <h1>{User.age}</h1>
-        <h1>{User.married.toString()}</h1>
-        <h1>{add(10, 40)}</h1>
-    </>
-
-};
 
 root.render(
-    Gretting()
-);
+    <>
+        <Gretting />
+        <UserCard />
+        <Product />
+        <Navbar />
+    </>
+)
