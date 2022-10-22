f612b41e0670c05215eb2619741ed139564fa982
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-22 17:39:05 -0500

    Learning more syntax

--- my-first-project/src/index.js
+++ my-first-project/src/index.js
@@ -4,16 +4,33 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Gretting () {
-    const married = false;
-    const age = 18;
+function Gretting() {
+    // const married = true;
+    // const age = 18;
 
     // if (married) {
     //     return <h1> Estoy casado!</h1>
     // };
 
-    //Ternary operators
-    return <h1>{married ? 'Estoy casado!' : 'No estoy casado'}</h1>
+    //Ternary operators.
+    // return <h1>{married ? 'Estoy casado! 🥴' : 'No estoy casado'}</h1>
+
+    const User = {
+        name: 'John',
+        age: 18,
+        married: true
+    };
+
+    function add(x, y) {
+        return x + y;
+    };
+
+    return <>
+        <h1>{User.name}</h1>
+        <h1>{User.age}</h1>
+        <h1>{User.married.toString()}</h1>
+        <h1>{add(10, 40)}</h1>
+    </>
 
 };
 
