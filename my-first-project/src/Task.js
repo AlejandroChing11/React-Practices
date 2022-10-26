7b207565e422554a65a963043b0730fa5bfd1d3c
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-25 22:02:25 -0500

    Fixing problems with css

--- my-first-project/src/Task.js
+++ my-first-project/src/Task.js
@@ -2,12 +2,11 @@ import './task.css'
 
 export function TaskCard({ ready }) {
 
-    // const cardStyles = { background: "#202020", color: "#fff", padding: '20px' }
-    // const titleStyles = { fontWeight: "lighter" }
-
-
     return <div className='card'>
         <h1>Mi primer tarea</h1>
-        <span className={ready ? 'bg-green' : 'bg-red'}>{ready == true ? "Tarea realizada" : "tarea no realizada"}</span>
+        <span className={ready ? 'bg-green' : 'bg-red'}>
+            {ready ? "Tarea realizada" : "tarea no realizada"}
+        </span>
     </div>
+
 };
