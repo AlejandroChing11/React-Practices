7cb61ade691addb1d7925165e106509c26c94a28
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-26 19:23:00 -0500

    Another way to create components

--- my-first-project/src/index.js
+++ my-first-project/src/index.js
@@ -4,15 +4,14 @@ import { Gretting, UserCard } from './Greeting';
 import Product, { Navbar } from './Product';
 import { Button } from './button.js';
 import { TaskCard } from './Task';
-
-
+import { Saludar } from './Saludar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
     <>
-        <TaskCard ready={false} />
 
+        <Saludar/>
 
 
 
