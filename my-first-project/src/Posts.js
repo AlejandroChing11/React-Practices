ea9d637cd4318c309ee57677d5979477c2c85a47
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-26 20:24:55 -0500

    Icons in react

--- my-first-project/src/Posts.js
+++ my-first-project/src/Posts.js
@@ -1,12 +1,15 @@
-export const Post= () => {
+import { VscGlobe } from 'react-icons/vsc'
+
+
+export const Post = () => {
     return <button onClick={() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-           .then(data => console.log(data))
-           .catch(error => console.error(error))
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error))
     }}>
-        
 
+        <VscGlobe/>
         Traer Datos
     </button>
 };
