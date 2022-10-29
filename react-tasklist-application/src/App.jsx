2380ad7e9d5f3d7ce316a15e00d3f30cb124e1ea
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-28 19:48:06 -0500

    moving the tasks array to appjsx (main component)

--- react-tasklist-application/src/App.jsx
+++ react-tasklist-application/src/App.jsx
@@ -1,11 +1,19 @@
 import TaskList from "./TaskList";
 import { TaskForm } from "./TaskForm";
+import { tasks as data } from "./task";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [task, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
   return (
     <>
-      <TaskForm/>
-      <TaskList />
+      <TaskForm />
+      <TaskList tasks={data}/>
     </>
   );
 }
--- react-tasklist-application/src/TaskList.jsx
+++ react-tasklist-application/src/TaskList.jsx
@@ -1,24 +1,15 @@
-import { tasks as data} from "./task";
-import { useState, useEffect } from "react";
 
-
-function TaskList() {
-  const [task, setTasks] = useState([]);
-
-  useEffect(() => {
-    setTasks(data);
-  }, []);     
-
-  if (task.length === 0) {
-    return <h1>No hay tareas aun</h1>
+function TaskList(props) {
+  if (props.tasks.length === 0) {
+    return <h1>No hay tareas aun</h1>;
   }
 
   return (
     <div>
-      {task.map((task) => (
+      {props.tasks.map((task) => (
         <div key={task.id}>
-            <h1>{task.title}</h1>
-            <p>{task.description}</p>
+          <h1>{task.title}</h1>
+          <p>{task.description}</p>
         </div>
       ))}
     </div>
