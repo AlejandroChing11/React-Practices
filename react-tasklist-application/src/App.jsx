7c03e2398973d8a734d0ac6b059798d1bdd50b0e
Alejandro Ching <alejandroching2004@hotmail.com>
2022-11-04 01:35:15 -0500

    Adding css to out project

--- react-tasklist-application/src/App.jsx
+++ react-tasklist-application/src/App.jsx
@@ -2,13 +2,13 @@ import { TaskList } from "./components/TaskList";
 import Taskform from "./components/Taskform";
 
 function App() {
-
-
   return (
-    <>
-      <Taskform/>
-      <TaskList/>
-    </>
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-10">
+        <Taskform />
+        <TaskList />
+      </div>
+    </main>
   );
 }
 
