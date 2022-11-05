82175740803d6a96c23e44630ed37e25e37272c6
Alejandro Ching <alejandroching2004@hotmail.com>
2022-11-05 18:18:39 -0500

    Starting a new project!

--- advanced-practice/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-module.export = {
-    output: {
-        filename: 'app.bunddle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin()
-    ]
-}
