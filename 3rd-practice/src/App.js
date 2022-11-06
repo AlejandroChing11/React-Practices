2698acd3fdcd95460665d3e775c80258721c9fa8
Alejandro Ching <alejandroching2004@hotmail.com>
2022-11-05 19:04:10 -0500

    Learning states and adding images

--- 3rd-practice/src/App.js
+++ 3rd-practice/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [linea1, setLinea1] = useState("Hola mundo");
+  const [linea2, setLinea2] = useState("Estoy aprendiendo React");
+  const [image, setImage] = useState("");
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={(e) => {
+        setImage(e.target.value)
+      }}>
+        <option value="Casa">Casa en llamas</option>
+        <option value="futurama">futurama</option>
+        <option value="history">historty chanel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">philosoraptor</option>
+        <option value="smart guy">Smart Guy</option>
+      </select> <br />
+
+      <input onChange={(e) => {
+        setLinea1(e.target.value);
+      }} type="text" placeholder="Linea 1" /> <br />
+      <input onChange={(e) => {
+        setLinea2(e.target.value);
+      }} type="text" placeholder="Linea 2" /> <br />
+      <button>Export!</button> <br />
+
+
+      <div>
+        <span>{linea1}</span> <br />
+        <span>{linea2}</span>
+        <img src="" />
+      </div>
+
     </div>
   );
 }
